d98e6d8f6c2a07c8a8671847de48b306b0e27ae4
serg-and <serge.andriessen@gmail.com>
2023-07-28 17:33:16 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from 'react'
+
+const et = new EventTarget()
+
+export function createStore<T>(initialValue: T, name: string) {
+  const store = initialValue
+  const eventType = `${name.toUpperCase()}_WATCHI_UPDATE`
+  const event = new Event(eventType)
+
+  function addWatch(callback: () => unknown) {
+    et.addEventListener(eventType, callback)
+    return () => et.removeEventListener(eventType, callback)
+  }
+
+  function triggerWatch() {
+    et.dispatchEvent(event)
+  }
+
+  function useWatch<SelectRes>(select: (store: T) => SelectRes) {
+    const [state, setState] = useState<SelectRes>(select(store))
+    const stateRef = useRef(state)
+
+    useEffect(() => {
+      const removeWatch = addWatch(() => {
+        const selectRes = select(store)
+
+        if (!Object.is(selectRes, stateRef.current)) {
+          setState(selectRes)
+          stateRef.current = selectRes
+        }
+      })
+
+      return removeWatch
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return state
+  }
+
+  return { store, addWatch, triggerWatch, useWatch }
+}
+
+type Store = { a?: { b: { l: { id: string; n: number }[]; s: string } } }
+const { store, addWatch, triggerWatch, useWatch } = createStore<Store>({}, 'mystore')
+const removeWatch = addWatch(() => console.log('store updated'))
+triggerWatch()
+removeWatch()
+triggerWatch()
+
+function MyComponent() {
+  const b = useWatch((store) => store.a?.b)
+  
+  return null
+}
