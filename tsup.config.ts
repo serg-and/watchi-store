90473edfb9408007177d3151249ce5733307e892
serg-and <serge.andriessen@gmail.com>
2023-10-12 18:49:58 +0200

    fix import CommonJS

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,7 @@ export const tsup: Options = {
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
   format: ['cjs', 'esm'], // generate cjs and esm files
+  outExtension: (ctx) => ({ js: ctx.format === 'esm' ? '.mjs' : '.js', dts: '.d.ts'}),
   minify: true,
   bundle: true,
   skipNodeModulesBundle: true,
